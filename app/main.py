be3c98db565d87762588539967da1ebf8511fab6
Futa HIRAKOBA <korosuke613613@gmail.com>
2017-11-28 20:15:55 +0900

    評価関数の試作

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from spec2test import Imporwords
+from spec2test import Imporwords, uniqword
 
 
 def main():
     imporwords = Imporwords(imporwords_dir_path_="./resource/imporwords/")
     imporwords.generate_imporwords()
+    uniqword.gen("./resource/wakachi/")
 
 
 if __name__ == '__main__':
--- app/spec2test/uniqword.py
+++ app/spec2test/uniqword.py
@@ -1,16 +1,35 @@
-import csv
+import os
 
 
-def uniq_word(path):
+def create_file_list(dir_path):
+    """ファイルリストを生成する"""
+
+    def is_test_file(path_):
+        """テストケースのファイルを除外するかどうかを判断する関数"""
+        return path_[:4] == "test"
+
+    return [dir_path + path
+            for path in os.listdir(dir_path)
+            if path[-len(".meishi.wakachi"):] == ".meishi.wakachi"
+            and is_test_file(path)]
+
+
+def gen(dir_path):
+    test_file_list = create_file_list(dir_path)
+    for file in test_file_list:
+        gen_simple(file)
+
+
+def gen_simple(path):
     with open(path, "r", encoding="utf_8_sig") as f:
         word_list = []
         for row in f:
             word_list.extend(row.split())
         sorted_array2d = set(word_list)
-        with open(path + "uniq.csv", "w", encoding="utf_8_sig") as f:
+        with open(path + ".uniq.csv", "w", encoding="utf_8_sig") as f:
             for word in sorted_array2d:
                 f.write(word + "\n")
 
 
 if __name__ == "__main__":
-    uniq_word("../resource/wakachi/test_forcus.txt.meishi.wakachi")
+    gen("../resource/wakachi/")
