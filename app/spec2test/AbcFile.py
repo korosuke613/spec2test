b4fc584bf41a3a85707007203a60b7d251a9acd7
Futa HIRAKOBA <korosuke613613@gmail.com>
2017-11-30 23:42:12 +0900

    アノテーションの追加

--- app/spec2test/AbcFile.py
+++ app/spec2test/AbcFile.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
 class AbcBase(ABC):
@@ -10,61 +10,61 @@ class AbcBase(ABC):
         self.__oe = None
 
     @property
-    def work_dir_path(self):
+    def work_dir_path(self) -> str:
         return self.__wdp
 
     @work_dir_path.setter
-    def work_dir_path(self, path):
+    def work_dir_path(self, path: str):
         self.__wdp = path
 
     @property
-    def input_dir_path(self):
+    def input_dir_path(self) -> str:
         """ファイルを読み込むディレクトリ"""
         return self.work_dir_path + self.__idp
 
     @input_dir_path.setter
-    def input_dir_path(self, path):
+    def input_dir_path(self, path: str):
         self.__idp = path
 
     @property
-    def output_dir_path(self):
+    def output_dir_path(self) -> str:
         """ファイルを書き出すディレクトリ"""
         return self.work_dir_path + self.__odp
 
     @output_dir_path.setter
-    def output_dir_path(self, path):
+    def output_dir_path(self, path: str):
         """ファイルを書き出すディレクトリ"""
         self.__odp = path
 
     @property
-    def input_extension(self):
+    def input_extension(self) -> str:
         """入力ファイルの拡張子"""
         return self.__ie
 
     @input_extension.setter
-    def input_extension(self, extension):
+    def input_extension(self, extension: str):
         """入力ファイルの拡張子"""
         self.__ie = extension
 
     @property
-    def output_extension(self):
+    def output_extension(self) -> str:
         return self.__oe
 
     @output_extension.setter
-    def output_extension(self, extension):
+    def output_extension(self, extension: str):
         self.__oe = extension
 
     def set_path(self,
-                 work_dir_path_,
-                 input_dir_path_,
-                 output_dir_path_,
+                 work_dir_path_: str,
+                 input_dir_path_: str,
+                 output_dir_path_: str,
                  ):
         self.work_dir_path = work_dir_path_
         self.input_dir_path = input_dir_path_
         self.output_dir_path = output_dir_path_
 
     def set_extension(self,
-                      input_extension_,
-                      output_extension_):
+                      input_extension_: str,
+                      output_extension_: str):
         self.input_extension = input_extension_
         self.output_extension = output_extension_
