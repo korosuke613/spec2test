cff90890d3c2a4886192a2a59d670dda6cd23444
Futa HIRAKOBA <korosuke613613@gmail.com>
2018-01-06 05:15:47 +0900

    もろもろの名前を修正

--- app/gen_all.py
+++ app/gen_all.py
@@ -1,12 +1,12 @@
 import gen_wakachi
-import gen_model
+import gen_word_vector
 import gen_tfidf
 import gen_imporwords
 
 
 def main():
     gen_wakachi.main()
-    gen_model.main()
+    gen_word_vector.main()
     gen_tfidf.main()
     gen_imporwords.main()
 
