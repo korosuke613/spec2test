0055bde0d3cb7a748a3a2d3028d2a765b168d726
Futa HIRAKOBA <korosuke613613@gmail.com>
2018-01-09 11:52:00 +0900

    Evaluationクラスを継承するように変更

--- app/spec2test/evaluation.py
+++ app/spec2test/evaluation.py
@@ -1,22 +1,38 @@
 from directory import Directory
-from iomanager import IOManagerDirectory
 
 
-class Evaluation(IOManagerDirectory):
+class Evaluation:
     def __init__(self,
-                 input_dir_: Directory,
-                 output_dir_: Directory):
-        super().__init__(input_dir_, output_dir_)
+                 eval_a_: Directory,
+                 eval_b_: Directory,
+                 is_import=True):
+        self.eval_a = eval_a_
+        self.eval_b = eval_b_
+        if is_import:
+            self.eval_a.import_files()
+            self.eval_b.import_files()
 
-    def generate(self):
+    def compare(self):
+        raise NotImplementedError
+
+
+class EvaluationTestcase(Evaluation):
+    def __init__(self,
+                 eval_a_: Directory,
+                 eval_b_: Directory,
+                 is_import=True):
+        super().__init__(eval_a_, eval_b_, is_import)
+
+    def compare(self):
         raise NotImplementedError
 
 
 def main():
-    input_dir = Directory("./", ".txt")
-    output_dir = Directory("./", ".txt")
-    evaluation = Evaluation(input_dir, output_dir)
+    eval_a = Directory("./", ".txt")
+    eval_b = Directory("./", ".txt")
+    evaluation = Evaluation(eval_a, eval_b)
     print(evaluation)
+    evaluation.compare()
 
 
 if __name__ == '__main__':
