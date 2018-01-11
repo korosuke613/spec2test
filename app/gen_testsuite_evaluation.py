5972ef24d6cdb27986086cc1f4d30c53a5feebba
Futa HIRAKOBA <korosuke613613@gmail.com>
2018-01-11 20:42:12 +0900

    評価用のテストケース生成スクリプトを作成

---
+++ app/gen_testsuite_evaluation.py
@@ -0,0 +1,27 @@
+from spec2test import TestSuite
+
+
+def main():
+    testsuite = TestSuite(units_=100,
+                          learn_result_="model_iter_u100_e100_p104_11583",
+                          output_path="./resource/testsuite/simple_ptb/")
+    testsuite.length = 30
+    testsuite.load_vocabularies()
+    testsuite.load_vector()
+    generater = testsuite.load_imporwords()
+    num = 1
+    threshold = 0.0
+    for filename, impolist in generater:
+        print("create {0}".format(filename))
+        impolist = [impo[0] for impo in impolist]
+        testsuite_list, score_list = testsuite.create_testsuite(impolist,
+                                                                threshold_=threshold,
+                                                                num_=num,
+                                                                is_prime=False)
+        testsuite.create_csv(filename, testsuite_list, score_list)
+        print("done {0}".format(filename))
+        print()
+
+
+if __name__ == '__main__':
+    main()
